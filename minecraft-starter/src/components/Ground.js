97437d3245cbcff996948b8efc9572d2a455235d
Marcel Hinderlich <m.hinderlich@reply.de>
2022-02-25 23:49:09 +0100

    useMemo fix for ground rendering

--- minecraft-starter/src/components/Ground.js
+++ minecraft-starter/src/components/Ground.js
@@ -1,19 +1,26 @@
-import React, {useState} from 'react';
+import React, {useMemo} from 'react';
 import {usePlane} from '@react-three/cannon'
 import {TextureLoader, RepeatWrapping} from 'three';
 import grass from '../images/grass.jpg';
 import {useStore} from "../hooks/useStore";
 
+
+
 export const Ground = (props) => {
     const [ref] = usePlane(() => ({rotation: [-Math.PI / 2, 0, 0], ...props}));
-    const texture = new TextureLoader().load(grass);
-    texture.wrapS = RepeatWrapping;
-    texture.wrapT = RepeatWrapping;
-    texture.repeat.set(100, 100);
+
     const [addCube, activeTexture] = useStore((state) => [
         state.addCube,
         state.texture
     ])
+
+    const texture = useMemo(() => {
+        const t = new TextureLoader().load(grass)
+        t.wrapS = RepeatWrapping
+        t.wrapT = RepeatWrapping
+        t.repeat.set(100, 100)
+        return t
+    }, [])
 
     return (
         <mesh

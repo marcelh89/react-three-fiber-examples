0e17b00c10f60fa14f1a18466c24390ca75ea9d2
Marcel Hinderlich <m.hinderlich@reply.de>
2022-02-26 18:24:33 +0100

    disabled hud, added static menu

--- minecraft-starter/src/App.js
+++ minecraft-starter/src/App.js
@@ -7,22 +7,25 @@ import {Ground} from './components/Ground';
 import {Player} from './components/Player';
 import Cubes from "./components/Cubes";
 import {Hud} from "./components/Hud";
-
+import Menu from "./Menu";
 
 function App() {
 
     return (
-        <Canvas shadowMap sRGB>
-            <Sky sunPosition={[100, 20, 100]}></Sky>
-            <ambientLight intensity={0.25}/>
-            <pointLight castShadow intentity={0.7} position={[100, 100, 100]}/>
-            <Hud position={[0, 0, -2]} />
-            <Physics gravity={[0, -30, 0]}>
-                <Ground position={[0, 0.5, 0]}/>
-                <Player position={[0, 3, 10]}/>
-                <Cubes />
-            </Physics>
-        </Canvas>
+        <>
+            <Canvas shadowMap sRGB>
+                <Sky sunPosition={[100, 20, 100]}></Sky>
+                <ambientLight intensity={0.25}/>
+                <pointLight castShadow intentity={0.7} position={[100, 100, 100]}/>
+                {/*<Hud position={[0, 0, -2]}/>*/}
+                <Physics gravity={[0, -30, 0]}>
+                    <Ground position={[0, 0.5, 0]}/>
+                    <Player position={[0, 3, 10]}/>
+                    <Cubes/>
+                </Physics>
+            </Canvas>
+            <Menu/>
+        </>
     )
 }
 
